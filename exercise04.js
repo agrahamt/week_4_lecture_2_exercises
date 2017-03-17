730b09c64652fb674c1c510e8d35e51347a4d5ae
Andrew Taylor <agrahamt@Andrews-MacBook-Air.local>
2017-03-17 17:26:39 -0500

    added exercise04.js

---
+++ exercise04.js
@@ -0,0 +1,51 @@
+// 4.  Write a small program that has a line of code that does each
+// of the following and prints out the result:
+
+// Add two numbers together.
+// EG: eight plus fifteen
+console.log (8 + 15)
+
+
+// Add three numbers together using the addition operator.
+// EG: eight plus fifteen plus negative one hundred
+console.log (8 + 15 + -23)
+// Add a positive and a negative number.
+// EG: negative five plus thirty two point eight
+console.log (-10 + 5)
+// Subtract two numbers.
+// EG: thirty five minus two point eight
+console.log(10 - 2)
+// Subtract three numbers.
+// EG: thirty five minus two point eight minus seventeen
+console.log (35 - 5 - 79)
+
+// Subtract a positive and a negative number.
+// EG: negative five plus thirty two minus eight
+console.log (7 - 15 - - 32)
+// Combine the addition and subtraction operators with three numbers.
+// EG: seventy five plus negative thirty five minus two point eight
+console.log (75 + -35 - 2.8)
+
+// Multiply two numbers
+// EG: six times negative zero point one
+console.log (15 * -3.5)
+// Multiply three numbers where one is negative
+// EG: negative eight times five times one billion point eight
+console.log (-5 * 5 * 5)
+
+// Divide two numbers
+// EG: five divided by negative twelve.
+console.log (5/3)
+// Divide three numbers
+// EG: twenty five divided by negative twelve divided by two
+console.log (27 / 9 / 3)
+// Find the remainder of dividing two integers
+// EG: The remainder left when dividing twenty five by four
+console.log (7 % 3)
+
+// An expression that uses each of the following:
+// Negative and Positive Numbers, Integers and Doubles, Parenthesis,
+// Addition, Subtraction, Multiplication, Division, and Modulo.
+// EG: The remainder of five plus negative four minus negative
+// four times three divided by two point three, divided by nine
+console.log ((7- -3)^2 * 52) + 25 % (45 / 9)
