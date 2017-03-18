d685da90bb26c1831e25afa5b68df727bc2b7538
Andrew Taylor <agrahamt@Andrews-MacBook-Air.local>
2017-03-18 11:42:05 -0500

    added exercise05.js

---
+++ exercise05.js
@@ -0,0 +1,10 @@
+//I will work on my class assignments if I have prepared my presentation for UH and have my computer and am in class but not if I am completely up to date with my work or all-knowledgeable.
+
+var preparedUHPresentation = false
+var caughtUp = false
+var inClass = true
+var haveComputer = true
+var allKnowing = false
+
+//will work on FEE exercises
+log(preparedUHPresentation && inClass && haveComputer && (!caughtUp || !allKnowing))
